c13e6ab1cee6431465db7739a626695d48a364c3
tehmeerali786 <freelanceali786@gmail.com>
2024-12-30 15:36:46 +0500

    Chapter 7

---
+++ Chapter 7/static/js/index8.js
@@ -0,0 +1,178 @@
+/*
+   VARIABLES  
+ */
+
+// Connect to Websockets Server (SocialNetworkConsumer)
+const myWebSocket = new WebSocket(`${document.body.dataset.scheme === "http" ? "ws": "wss" }://${ document.body.dataset.host}/ws/blog/`);
+
+/*
+	FUNCTIONS
+*/
+
+/**
+ * Send data to Websockets over 
+ * @param {string} message
+ * @param {WebSocket} Websocket
+ * @return {void}
+ * 
+ **/
+
+function sendData(message, webSocket) {
+	webSocket.send(JSON.stringify(message))
+}
+
+/**
+ * Send page to update the page
+ * @param {Event} event
+ * @returns {void}
+ * 
+ **/
+
+function changePage(event) {
+	event.preventDefault();
+	sendData({
+		action: "Change page",
+		data: {
+			page: event.target.dataset.target,
+			id: event.target.dataset?.id
+		}
+	});
+}
+
+/*
+	 EVENTS
+*/
+
+/**
+ * Send message to WebSockets server to change the page
+ * @param {WebSocket} webSocket
+ * @return {void}
+ * 
+ **/
+
+function setEventsNavigation(webSocket) {
+	// Navigation
+	document.querySelectorAll(".nav__link--page").forEach(link => {
+		link.removeEventListener("click", changePage, false);
+		link.addEventListener("click", changePage, false);
+	});
+}
+
+
+// Event when a new message is received by WebSockets 
+myWebSocket.addEventListener("message", (event) => {
+	// Parse the data received 
+	const data = JSON.parse(event.data);
+	// Renders the HTML received from the Consumer
+	const selector = document.querySelector(data.selector);
+	// We only allow all users to render if we receive a broadcast as true and it is at the same url.
+	if (
+
+		data.broadcast === undefined ||
+		!data.broadcast ||
+		(data.broadcast && data.url === document.location.pathname)
+
+		) {
+
+		// If append is received, it will be appended. Otherwise the entire DOM will be replaced.
+
+		if (data.append) {
+			selector.innerHTML += data.html;
+		}
+
+		else {
+
+			selector.innerHTML = data.html;
+		    // Update URL 
+		}
+
+	}
+	
+	/**
+	 *  Reassigns the events of the newly renderd HTML
+	 * 
+	 **/
+	updateEvents();
+	
+});
+
+/**
+ * Event to add a next page with pagination
+ * @param event
+ * 
+ **/
+function addNextPaginator(event) {
+	const nextPage = event.target.dataset.nextPage;
+	sendDate({
+		action: "Add next posts",
+		data: {
+			page: nextPage
+		},
+	}, myWebSocket);
+}
+
+
+/**
+ * Event that asks a consumer to add a new comment
+ * @param event
+ * 
+ **/
+
+function addComment(event) {
+	event.preventDefault();
+	const author = event.target.querySelector("#author").value;
+	const content = event.target.querySelector("#content").value;
+	const postId = event.target.dataset.postId;
+	sendData({
+
+		action: "Add comment";
+		data: {
+
+			author: author,
+			content: content,
+			post_id: postId,
+		}
+	}, myWebSocket);
+}
+
+/**
+ * Event to request a search
+ * @param event
+ * 
+ **/
+function search(event) {
+	event.preventDefault();
+	const search = event.target.querySelector("#search").value;
+	alert(search);
+    sendData({
+        action: "Search",
+        data: {
+            search: search
+        },
+    }, myWebSocket);
+}
+
+/**
+ * Update events in every page
+ * return {void}
+ * 
+ **/
+function updateEvents() {
+		// Nav
+		setEventsNavigation(myWebSocket);
+		// Search form
+		const searchForm = document.querySelector("#search-form");
+		searchForm.removeEventListener("submit", search, false);
+		searchForm.addEventListener("submit", search, false);
+
+
+	
+	}
+
+
+/*
+	Initialize 
+
+*/
+
+updateEvents();
